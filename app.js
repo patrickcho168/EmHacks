0359b19b80f13361da3f0496abc763a1f7280f3d
Julietta Li <lidu.julietta@gmail.com>
2015-11-08 02:32:06 -0800

    added in my code in wheretogo

--- app.js
+++ app.js
@@ -212,12 +212,16 @@ app.post("/getquotes", function(request, response) {
   // NOTE: 
   // des formats: anywhere
   // od formats: anytime, yyyy-mm, yyyy-mm-dd
-  var webpath = 'http://api.skyscanner.net/apiservices/xd/browsequotes/v1.0/SG/USD/en-US/sfo' + '/' + search_params.des + '/' + search_params.od + '/anytime?apikey=ah473367287496555171637201591562&callback=cb';
+
+  // var webpath = 'http://api.skyscanner.net/apiservices/xd/browsequotes/v1.0/SG/USD/en-US/sfo/' + search_params.des + '/' + search_params.od + '/anytime?apikey=ah473367287496555171637201591562&callback=cb';
+
+  var webpath = 'http://api.skyscanner.net/apiservices/xd/browsequotes/v1.0/SG/USD/en-US/sfo/sg/anytime?apikey=ah473367287496555171637201591562&callback=cb';
+
   console.log("#############webpath is: " + webpath + "############");
   httprequest(webpath, function (error, response2, body2) {
     if (!error && response.statusCode == 200) {
       console.log(body2); // Show the HTML for the Google homepage.
-      response.render('browse_quotes',{webpath: webpath});
+      response.render('index',{webpath: webpath});
     }
   })
 });
